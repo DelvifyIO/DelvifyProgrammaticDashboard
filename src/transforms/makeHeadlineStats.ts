6b59b19977340fddce1be635d92f0ec71259d54f
hooiyung <yung@delvify.io>
2020-08-18 17:42:51 +0800

    Update CTR, CPC, CPM, CPA calculation

--- src/transforms/makeHeadlineStats.ts
+++ src/transforms/makeHeadlineStats.ts
@@ -23,23 +23,15 @@ export default function makeHeadlineStats(metrics, campaignId, dateRangeDays = 3
   function addToHistorical(row): void {
     historical.impressions += Number(row[IMPRESSIONS]);
     historical.clicks += Number(row[CLICKS]);
-    historical.ctr += Number(row[CLICK_RATE].slice(0, -1));
     historical.spend += Number(row[SPEND]);
     historical.conversions += Number(row[TOTAL_CONVERSIONS]);
-    historical.cpc += Number(row[CPC]);
-    historical.cpm += Number(row[CPM]);
-    historical.cpa += Number(row[CPA]);
   }
 
   function addToRecent(row): void {
     recent.impressions.val += Number(row[IMPRESSIONS]);
     recent.clicks.val += Number(row[CLICKS]);
-    recent.ctr.val += Number(row[CLICK_RATE].slice(0, -1));
     recent.spend.val += Number(row[SPEND]);
     recent.conversions.val += Number(row[TOTAL_CONVERSIONS]);
-    recent.cpc.val += Number(row[CPC]);
-    recent.cpm.val += Number(row[CPM]);
-    recent.cpa.val += Number(row[CPA]);
   }
   return new Promise((resolve, reject) => {
     metrics.forEach((row) => {
@@ -51,9 +43,16 @@ export default function makeHeadlineStats(metrics, campaignId, dateRangeDays = 3
       }
       difference > dateRangeDays ? addToHistorical(row) : addToRecent(row);
     });
+    historical.cpc = historical.clicks ? historical.spend / historical.clicks : 0;
+    historical.ctr = historical.impressions ? historical.clicks / historical.impressions : 0;
+    historical.cpm = (historical.impressions ? historical.spend / historical.impressions : 0) * 1000;
+    historical.cpa = historical.conversions ? historical.spend / historical.conversions : 0;
 
-    recent.ctr.val = recent.ctr.val / dateRangeDays;
-    historical.ctr /= dateRangeDays;
+
+    recent.cpc.val = recent.clicks.val ? recent.spend.val / recent.clicks.val : 0;
+    recent.ctr.val = recent.impressions.val ? recent.clicks.val / recent.impressions.val : 0;
+    recent.cpm.val = (recent.impressions.val ? recent.spend.val / recent.impressions.val : 0) * 1000;
+    recent.cpa.val = recent.conversions.val ? recent.spend.val / recent.conversions.val : 0;
 
     recent.impressions.change = (recent.impressions.val - historical.impressions) / recent.impressions.val * 100;
     recent.clicks.change = (recent.clicks.val - historical.clicks) / recent.clicks.val * 100;
